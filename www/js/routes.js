7bcef6310ff9cc7a4db04e946320d01069890042
Ali Rahimi <rahimi@seamlessinteraction.com>
2016-07-12 16:40:25 +0200

    inital commit

---
+++ www/js/routes.js
@@ -0,0 +1,95 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+  
+      
+        
+    .state('login', {
+      url: '/page1',
+      templateUrl: 'templates/login.html'
+    })
+        
+      
+    
+      
+    .state('menu', {
+      url: '/side-menu21',
+      abstract:true,
+      templateUrl: 'templates/menu.html'
+    })
+      
+    
+      
+        
+    .state('tabsController.news', {
+      url: '/page4',
+      views: {
+        'tab1': {
+          templateUrl: 'templates/news.html'
+        }
+      }
+    })
+        
+      
+    
+      
+        
+    .state('tabsController.cartTabDefaultPage', {
+      url: '/page5',
+      views: {
+        'tab2': {
+          templateUrl: 'templates/cartTabDefaultPage.html'
+        }
+      }
+    })
+        
+      
+    
+      
+        
+    .state('tabsController.cloudTabDefaultPage', {
+      url: '/page6',
+      views: {
+        'tab3': {
+          templateUrl: 'templates/cloudTabDefaultPage.html'
+        }
+      }
+    })
+        
+      
+    
+      
+    .state('tabsController', {
+      url: '/page3',
+      abstract:true,
+      templateUrl: 'templates/tabsController.html'
+    })
+      
+    
+      
+        
+    .state('signup', {
+      url: '/page7',
+      templateUrl: 'templates/signup.html'
+    })
+        
+      
+    ;
+
+  // if none of the above states are matched, use this as the fallback
+  
+  $urlRouterProvider.otherwise('/page1');
+  
+
+  
+
+});
